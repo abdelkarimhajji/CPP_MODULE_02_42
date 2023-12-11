3df9cdb0955711092c9aa3ef722915045aef35ba
abdelkarim hajji <hajji.karim.solicode@gmail.com>
2023-12-11 10:22:58 +0100

    i add ex01

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/08 17:28:38 by ahajji            #+#    #+#             */
+/*   Updated: 2023/12/11 10:16:27 by ahajji           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+ #include "Fixed.hpp"
+#include <cmath>
+Fixed::Fixed()
+{
+    this->value = 0;
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const int integer)
+{
+    // this->value = integer << this->fractionalBits;
+    this->value = integer * static_cast<int>(pow(2, this->fractionalBits));
+}
+
+Fixed::Fixed(Fixed &copy)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    this->value = copy.getRawBits();
+}
+
+void Fixed::set(int value)
+{
+    this->value = value;
+}
+
+void Fixed::print()
+{
+    // this->value = value >> this->fractionalBits;
+    std::cout << this->value  << "\n";
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->value = other.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits( void ) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->value;
+}
+
+void Fixed::setRawBits( int const raw )
+{
+    this->value  =  raw;
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/08 17:28:36 by ahajji            #+#    #+#             */
+/*   Updated: 2023/12/10 11:58:31 by ahajji           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class Fixed
+{
+  private:
+    int value;
+    static int const fractionalBits = 8;
+
+  public:
+    Fixed();
+    ~Fixed();
+    Fixed(Fixed &copy);
+    Fixed(const int integer);
+    void set(int value);
+    void print();
+    Fixed& operator=(const Fixed& other);
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
+};
